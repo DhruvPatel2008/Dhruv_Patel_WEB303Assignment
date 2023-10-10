ce948606a36c3edcdb18ae83e57fc1840e2b648a
DhruvPatel2008 <145496031+DhruvPatel2008@users.noreply.github.com>
2023-10-10 10:05:12 -0400

    Update script.js

--- WEB303As04/js/script.js
+++ WEB303As04/js/script.js
@@ -1,56 +1,75 @@
-// Assignment #4
-// Dhruv patel 
 
-document.addEventListener('DOMContentLoaded', () => {
-
-  const checkGeolocation = () => {
-    if(!navigator.geolocation) {
-      showMessage('Geolocation is not supported by your browser.');
-      return;
-    }
-
-    navigator.geolocation.getCurrentPosition(handleSuccess, handleError);
-  }
-
-  const handleSuccess = position => {
-    const {latitude, longitude} = position.coords;
-
-    showMessage(`Latitude: ${latitude}°, Longitude: ${longitude}°`);
+$(function () {
     
-    try {
-      const stored = localStorage.getItem('location');
-      if(stored) {
-        const {prevLat, prevLng} = JSON.parse(stored);
-        showMessage(`Previous Latitude: ${prevLat}°, Previous Longitude: ${prevLng}°`, 'h3');
-        
-        const distance = calculateDistance(latitude, longitude, prevLat, prevLng);
-        showMessage(`You traveled ${distance.toFixed(2)} meters since your last visit.`);
-      } else {
-        showMessage('Welcome for the first time!', 'h3');  
+  if (!navigator.geolocation) {
+      document.getElementById('locationhere').textContent = "Geolocation is not supported by your browser.";
+      return;
+  }
+  
+  function displayLocation(position) {
+      const currentLatitude = position.coords.latitude;
+      const currentLongitude = position.coords.longitude;
+  
+      document.getElementById('locationhere').textContent = `Latitude: ${currentLatitude} °, Longitude: ${currentLongitude} °`;
+  
+      try {
+          const storedLocation = JSON.parse(localStorage.getItem('location'));
+  
+          if (storedLocation) {
+              const distance = calcDistanceBetweenPoints(currentLatitude, currentLongitude, storedLocation.latitude, storedLocation.longitude);
+              const oldLocationElement = document.createElement('p');
+              oldLocationElement.textContent = `Previous Latitude: ${storedLocation.latitude} °, Previous Longitude: ${storedLocation.longitude} °`;
+  
+              const welcomeBackHeader = document.createElement('h3');
+              welcomeBackHeader.textContent = 'Welcome back!';
+  
+              const distanceElement = document.createElement('p');
+              distanceElement.textContent = `You traveled ${distance.toFixed(2)} meters since your last visit.`;
+  
+              const parentElement = document.getElementById('locationhere').parentNode;
+              parentElement.appendChild(oldLocationElement);
+              parentElement.appendChild(welcomeBackHeader);
+              parentElement.appendChild(distanceElement);
+          } else {
+              const firstTimeHeader = document.createElement('h3');
+              firstTimeHeader.textContent = 'Welcome for the first time!';
+              document.getElementById('locationhere').parentNode.appendChild(firstTimeHeader);
+          }
+  
+
+          localStorage.setItem('location', JSON.stringify({latitude: currentLatitude, longitude: currentLongitude}));
+      } catch (e) {
+          console.error("Error accessing or manipulating localStorage:", e);
       }
-
-      localStorage.setItem('location', JSON.stringify({prevLat: latitude, prevLng: longitude}));
-
-    } catch(err) {
-      console.error('Error accessing localStorage', err);
-    }
   }
-
-  const handleError = () => {
-    showMessage('You must allow geolocation to use this app.');
+  
+  function displayError() {
+      document.getElementById('locationhere').textContent = "You must allow geolocation to use this application.";
   }
+  
+  navigator.geolocation.getCurrentPosition(displayLocation, displayError);
+  
 
-  const showMessage = (msg, element='p') => {
-    const parent = document.getElementById('locationhere').parentNode;
-    const el = document.createElement(element);
-    el.textContent = msg;
-    parent.appendChild(el);
+
+
+  // DO NOT EDIT ANY CODE IN THIS FUNCTION DEFINTION
+  // function to calculate the distance in metres between two lat/long pairs on Earth
+  // Haversine formula - https://en.wikipedia.org/wiki/Haversine_formula
+  // Aren't those cool variable names? Yah gotta love JavaScript
+  function calcDistanceBetweenPoints(lat1, lon1, lat2, lon2) {
+      var toRadians = function (num) {
+          return num * Math.PI / 180;
+      }
+      var R = 6371000; // radius of Earth in metres
+      var φ1 = toRadians(lat1);
+      var φ2 = toRadians(lat2);
+      var Δφ = toRadians(lat2 - lat1);
+      var Δλ = toRadians(lon2 - lon1);
+
+      var a = Math.sin(Δφ / 2) * Math.sin(Δφ / 2) + Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
+      var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+      return (R * c);
   }
-
-  function calculateDistance(lat1, lon1, lat2, lon2) {
-    // Haversine formula
-  }
-
-  checkGeolocation();
-
 });
+
